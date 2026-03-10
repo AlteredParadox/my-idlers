b982348f211e97f0f297f5a12e372c7ee1da71a7
AlteredParadox <266677007+AlteredParadox@users.noreply.github.com>
2026-03-09 22:32:04 -0500

    fix font preloads

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,6 +14,8 @@
         <link rel="stylesheet" href="{{ asset('css/light.css') }}">
     @endif
 
+    <link rel="preload" href="{{ asset('webfonts/fa-solid-900.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('webfonts/fa-regular-400.woff2') }}" as="font" type="font/woff2" crossorigin>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     @yield('css_links')
@@ -25,6 +27,25 @@
         {{ $slot }}
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script>
+    (function() {
+        if (!document.fonts) return;
+        document.fonts.ready.then(function() {
+            var needed = [
+                {weight: '900', url: '{{ asset("webfonts/fa-solid-900.woff2") }}'},
+                {weight: '400', url: '{{ asset("webfonts/fa-regular-400.woff2") }}'}
+            ];
+            needed.forEach(function(f) {
+                if (!document.fonts.check(f.weight + ' 1em "Font Awesome 6 Free"', '\uf007')) {
+                    var face = new FontFace('Font Awesome 6 Free',
+                        'url(' + f.url + ') format("woff2")',
+                        {weight: f.weight, style: 'normal', display: 'swap'});
+                    face.load().then(function(loaded) { document.fonts.add(loaded); });
+                }
+            });
+        });
+    })();
+    </script>
     @yield('scripts')
 </body>
 </html>
